f445c61dd85c087736ef16919115443efb490200
Nikitoon <nyajin.nyakin@yandex.ru>
2020-09-06 17:21:10 +0300

    finally

--- src/store.js
+++ src/store.js
@@ -6,13 +6,13 @@ Vue.use(Vuex);
 const store = new Vuex.Store({
     state: {
         contacts: [
-            {id: 0, title: 'sad', phoneNumber: '1 800 111 222 333', editingName: false, editingPhone: false},
-            {id: 1, title: '123', phoneNumber: '1 800 111 222 333', editingName: false, editingPhone: false},
-            {id: 2, title: 'n111', phoneNumber: '1 800 111 222 333', editingName: false, editingPhone: false},
-            {id: 3, title: 'lol', phoneNumber: '1 800 111 222 333', editingName: false, editingPhone: false},
-            {id: 4, title: 'kek', phoneNumber: '1 800 111 222 333', editingName: false, editingPhone: false},
-            {id: 5, title: 'shrek', phoneNumber: '1 800 111 222 333', editingName: false, editingPhone: false},
-            {id: 6, title: 'gh', phoneNumber: '1 800 111 222 333', editingName: false, editingPhone: false},
+            {id: 0, title: 'Alexa', phoneNumber: '1 800 111 222 333', editingName: false, editingPhone: false},
+            {id: 1, title: 'Bruen Black', phoneNumber: '1 800 123 123 123', editingName: false, editingPhone: false},
+            {id: 2, title: 'Kanye West', phoneNumber: '1 808 100 200 300', editingName: false, editingPhone: false},
+            {id: 3, title: 'Swaroop', phoneNumber: '13 37', editingName: false, editingPhone: false},
+            {id: 4, title: 'Kyle Fly', phoneNumber: '1 800 101 202 303', editingName: false, editingPhone: false},
+            {id: 5, title: 'Vladimir Vladimirovich', phoneNumber: '7 900 111 22 33', editingName: false, editingPhone: false},
+            {id: 6, title: 'Big G', phoneNumber: '1 888 888 888 888', editingName: false, editingPhone: false},
             
         ],
 
